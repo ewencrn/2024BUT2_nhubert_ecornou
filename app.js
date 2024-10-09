904e337ac297fa2f65672f5c72af62cfd2812e93
ewencrn <ewencornou@orange.fr>
2024-10-09 11:09:46 +0200

    2

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.arguments(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+}) 
+
+app.use(function (req, res) { 
+    res.status(404).render("404");
+})
+
+app.listen(3000, function () {
+    console.log('Server running on port 3000');
+});
