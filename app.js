0de7ed84d20d73cffa4f6243e1c87c2ae95df8aa
ewencrn <ewencornou@orange.fr>
2024-11-29 14:04:10 +0100

    test ajt produit location en cours

--- app.js
+++ app.js
@@ -107,6 +107,75 @@ app.get('/information', (req, res) => {
 });
 ;
 
+app.get('/produit/ajouter', (req, res) => {
+    res.render('ajouter-produit'); // Rendre une vue avec le formulaire
+});
+
+app.post('/produit/ajouter', (req, res) => {
+    const { Nom, Image, Prix, Disponibilite } = req.body; // Données du formulaire
+    const query = `INSERT INTO materiel_sport (Nom, Image, Prix, Disponibilite) VALUES (?, ?, ?, ?)`;
+    bdd.query(query, [Nom, Image, Prix, Disponibilite], (err, results) => {
+        if (err) {
+            console.error('Erreur lors de l’ajout du produit :', err);
+            return res.status(500).send('Erreur serveur');
+        }
+        res.redirect('/materiel'); // Redirige l'agent vers la liste des matériels
+    });
+});
+
+
+app.post('/produit/supprimer/:id', (req, res) => {
+    const productId = req.params.id;
+
+    const checkQuery = `SELECT * FROM locations WHERE ProduitID = ? AND Statut = 'En cours'`;
+    bdd.query(checkQuery, [productId], (err, results) => {
+        if (err) {
+            console.error('Erreur lors de la vérification :', err);
+            return res.status(500).send('Erreur serveur');
+        }
+
+        if (results.length > 0) {
+            return res.status(400).send('Le produit est actuellement en location et ne peut pas être supprimé.');
+        }
+
+        const deleteQuery = `DELETE FROM materiel_sport WHERE ID = ?`;
+        bdd.query(deleteQuery, [productId], (err, results) => {
+            if (err) {
+                console.error('Erreur lors de la suppression :', err);
+                return res.status(500).send('Erreur serveur');
+            }
+            res.redirect('/materiel'); // Redirige vers la liste des matériels
+        });
+    });
+});
+
+
+app.get('/locations', (req, res) => {
+    const query = `SELECT * FROM locations WHERE Statut = 'En cours'`;
+    bdd.query(query, (err, results) => {
+        if (err) {
+            console.error('Erreur lors de la récupération des locations :', err);
+            return res.status(500).send('Erreur serveur');
+        }
+        res.render('locations', { locations: results });
+    });
+});
+
+
+app.post('/location/finaliser/:id', (req, res) => {
+    const locationId = req.params.id;
+
+    const query = `UPDATE locations SET Statut = 'Terminée' WHERE ID = ?`;
+    bdd.query(query, [locationId], (err, results) => {
+        if (err) {
+            console.error('Erreur lors de la finalisation :', err);
+            return res.status(500).send('Erreur serveur');
+        }
+        res.redirect('/locations'); // Redirige vers la liste des locations
+    });
+});
+
+
 
 app.use(function (req, res) { 
     res.status(404).render("404");
